85bd969df0e62898e1b1d44607df9ab40fed91cb
Lorelei <uriel26535897932384626433@proton.me>
2025-08-10 12:12:39 +0545

    some random stuff 1

---
+++ lists-and-stuff.py
@@ -0,0 +1,15 @@
+import random
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+for i in range(len(supplies)):
+    print(f"Index {i} in supplies is: {supplies[i]}")
+
+print('pens' not in supplies)
+print('apple' in supplies)
+
+write, pins, fire, bind = supplies
+
+print(fire)
+
+print(random.choice(supplies))
