04aee848637b61d4e73df013e754cb68a99cccae
Lorelei <uriel26535897932384626433@proton.me>
2025-08-10 10:42:58 +0545

    assertion

---
+++ assert.py
@@ -0,0 +1,7 @@
+ages = [12, 32, 23, 1,40, 239, 13]
+
+ages.reverse()
+
+print(ages)
+
+assert ages[0] <= ages[-1] # asserts that the first ages should be less than the last age (should give an assertion error.)
