6dd67d99607182c5f0a430f81a8264ffd83f9949
Lorelei <uriel26535897932384626433@proton.me>
2025-09-04 10:54:30 +0545

    random stuffs

---
+++ deeper-collections.py
@@ -0,0 +1,8 @@
+# nums = [1, 2, 3, 4, 5]
+
+# sqaures = [n*n for n in nums]
+# evens = [n for n in nums if n%2 == 0]
+# pairs = [(i, j) for i in range(3) for j in range (2)]
+
+# print(evens, pairs)
+
--- headsortails.py
+++ headsortails.py
@@ -3,7 +3,7 @@ import random
 heads = 0 
 tails = 0 
 
-for i in range(1,1000001):
+for i in range(1):
     if random.randint(0,1) == 0:
         heads += 1
     else:
